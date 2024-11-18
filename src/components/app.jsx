783df8ec208d87534395f587ed5b2cb90ca726d8
Blindeen <kuba.szmajda@gmail.com>
2024-11-18 18:03:24 +0100

    organize main app component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { getDevice } from 'framework7/lite-bundle';
+import React, { useState } from "react";
+import { getDevice } from "framework7/lite-bundle";
 import {
   f7,
   f7ready,
@@ -14,18 +14,18 @@ import {
   NavRight,
   Link,
   Block,
-  Button
-} from 'framework7-react';
-import cordovaApp from '../js/cordova-app';
+  Button,
+} from "framework7-react";
+import cordovaApp from "../js/cordova-app";
 
-import routes from '../js/routes';
-import store from '../js/store';
+import routes from "../js/routes";
+import store from "../js/store";
 
 const device = getDevice();
 
 const f7params = {
-  name: 'CinemaNow', // App name
-  theme: 'auto', // Automatic theme detection
+  name: "CinemaNow", // App name
+  theme: "auto", // Automatic theme detection
   // App store
   store: store,
   // App routes
@@ -43,26 +43,11 @@ const f7params = {
 };
 
 const MyApp = () => {
-  // Login screen demo data
-  const [ email, setEmail ] = useState('');
-  const [ password, setPassword ] = useState('');
-  const [ passwordConfirm, setPasswordConfirm ] = useState('');
-  const [ firstName, setFirstName ] = useState('');
-  const [ lastName, setLastName ] = useState('');
-
   const isAuthenticated = store.getters.isAuthenticated.value;
-
-  const clearRegisterData = () => {
-    setEmail('');
-    setPassword('');
-    setPasswordConfirm('');
-    setFirstName('');
-    setLastName('');
-  };
 
   f7ready(() => {
     if (!isAuthenticated) {
-      f7.loginScreen.open('#login-screen');
+      f7.loginScreen.open("#login-screen");
     }
     // Init cordova APIs (see cordova-app.js)
     if (f7.device.cordova) {
@@ -73,87 +58,67 @@ const MyApp = () => {
   });
 
   return (
-    <App { ...f7params }>
-
-      {/* Left panel with cover effect*/ }
-      <Panel left cover dark>
-        <View>
-          <Page>
-            <Navbar title="Left Panel"/>
-            <Block>Left panel content goes here</Block>
-          </Page>
-        </View>
-      </Panel>
-
-
-      {/* Right panel with reveal effect*/ }
+    <App {...f7params}>
+      {/* Right panel with reveal effect*/}
       <Panel right reveal dark>
         <View>
           <Page>
-            <Navbar title="Right Panel"/>
+            <Navbar title="Right Panel" />
             <Block>
-              <Button fill onClick={
-                () => {
-                  store.dispatch('logout', null).then(
-                    () => {
-                      clearRegisterData();
-                      f7.dialog.alert('Logout successful');
-                      f7.loginScreen.open('#login-screen');
-                    }
-                  ).catch(
-                    (error) => {
+              <Button
+                fill
+                onClick={() => {
+                  store
+                    .dispatch("logout", null)
+                    .then(() => {
+                      f7.dialog.alert("Logout successful");
+                      f7.loginScreen.open("#login-screen");
+                    })
+                    .catch((error) => {
                       console.log(error);
-                      f7.dialog.alert('We cannot log you out at this time. Please try again later.');
-                    }
-                  );
-                }
-              }>Logout</Button>
+                      f7.dialog.alert(
+                        "We cannot log you out at this time. Please try again later."
+                      );
+                    });
+                }}
+              >
+                Logout
+              </Button>
             </Block>
           </Page>
         </View>
       </Panel>
 
-
-      {/* Views/Tabs container */ }
       <Views tabs className="safe-areas">
-        {/* Tabbar for switching views-tabs */ }
         <Toolbar tabbar icons bottom>
-          <Link tabLink="#view-home" tabLinkActive iconIos="f7:house_fill" iconMd="material:home"
-                text="Home"/>
-          <Link tabLink="#view-catalog" iconIos="f7:square_list_fill" iconMd="material:view_list"
-                text="Catalog"/>
-          <Link tabLink="#view-settings" iconIos="f7:gear" iconMd="material:settings" text="Settings"/>
-          <Link tabLink="#view-login" iconIos="f7:gear" iconMd="material:perm_identity" text="Login"/>
+          <Link
+            tabLink="#view-home"
+            tabLinkActive
+            iconIos="f7:house_fill"
+            iconMd="material:home"
+            text="Home"
+          />
+          <Link
+            tabLink="#view-films"
+            iconIos="f7:square_list_fill"
+            iconMd="material:live_tv"
+            text="Films"
+          />
+          <Link
+            tabLink="#view-login"
+            iconIos="f7:gear"
+            iconMd="material:perm_identity"
+            text="Account"
+          />
         </Toolbar>
 
-        {/* Your main view/tab, should have "view-main" class. It also has "tabActive" prop */ }
-        <View id="view-home" main tab tabActive url="/"/>
+        <View id="view-home" main tab tabActive url="/" />
 
-        {/* Catalog View */ }
-        <View id="view-catalog" name="catalog" tab url="/catalog/"/>
+        <View id="view-films" name="films" tab url="/films/" />
 
-        {/* Settings View */ }
-        <View id="view-settings" name="settings" tab url="/settings/"/>
-
-        <View id="view-register" name="register" tab url="/register/"/>
-        <View id="view-login" name="login" tab url="/login/"/>
+        <View id="view-register" name="register" tab url="/register/" />
+        <View id="view-login" name="login" tab url="/login/" />
       </Views>
-
-      {/* Popup */ }
-      <Popup id="my-popup">
-        <View>
-          <Page>
-            <Navbar title="Popup">
-              <NavRight>
-                <Link popupClose>Close</Link>
-              </NavRight>
-            </Navbar>
-            <Block>
-              <p>Popup content goes here.</p>
-            </Block>
-          </Page>
-        </View>
-      </Popup>
     </App>
   );
 };
