1f1ce74af25aa12da4644263fc1245572ec2dabf
Blindeen <kuba.szmajda@gmail.com>
2024-11-29 16:25:37 +0100

    add rent film button with logic

--- src/pages/film.jsx
+++ src/pages/film.jsx
@@ -1,9 +1,13 @@
-import React, { useEffect, useState } from "react";
-import { Page, Navbar, BlockTitle, Block } from "framework7-react";
+import React, { useEffect, useState, useContext } from "react";
+import { Page, Navbar, BlockTitle, Block, Button, f7 } from "framework7-react";
 import { api } from "@/utils/api";
+import { MyContext } from "@/js/context.jsx";
 
 const FilmPage = (props) => {
+  const [isLoading, setIsLoading] = useState(false);
   const [film, setFilm] = useState({});
+  const { user } = useContext(MyContext);
+
   const filmId = props.f7route.params.id;
 
   const fetchFilm = async () => {
@@ -25,6 +29,30 @@ const FilmPage = (props) => {
       });
   };
 
+  const rentFilm = async () => {
+    setIsLoading(true);
+    await api
+      .post(`/films/${filmId}/rentals`)
+      .then(async (resp) => {
+        const { status } = resp;
+        const data = await resp.json();
+        if (status !== 200) {
+          const {
+            errors: { general },
+          } = data;
+          const [message] = general;
+
+          f7.dialog.alert(message);
+        } else {
+          f7.dialog.alert("Film has been rented");
+        }
+        setIsLoading(false);
+      })
+      .catch(() => {
+        f7.dialog.alert("An error occurred. Please try again later.");
+      });
+  };
+
   useEffect(() => {
     fetchFilm();
   }, []);
@@ -40,6 +68,20 @@ const FilmPage = (props) => {
       <Block>
         <img src={film.posterURL} alt={film.name} width={300} height={300} />
       </Block>
+      {user && (
+        <Block>
+          <Button
+            iconMaterial="shopping_cart"
+            onClick={rentFilm}
+            fill
+            round
+            raised
+            isLoading={isLoading}
+          >
+            Rent
+          </Button>
+        </Block>
+      )}
     </Page>
   );
 };
